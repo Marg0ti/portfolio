877f58f272b0ee5dd2213236b14a35bf48ecfe82
Marg0ti <165667924+Marg0ti@users.noreply.github.com>
2025-05-30 20:26:34 +0200

    modificaciones de css

--- src/pages/proyectos/Proyectos.jsx
+++ src/pages/proyectos/Proyectos.jsx
@@ -9,30 +9,34 @@ export default function Proyectos(){
                     <img src="/src/media/pictures/background1.jpg" alt="" />
                 </div>
                 <div className="proyecto-detalles">
-                    <div className="proyecto-codigo">
+                    <div className="proyecto-detalles2">
+                        <div className="proyecto-codigo">
                         <GitCode url="https://github.com/Marg0ti/"/>
-                    </div>
-                    <div className="proyecto-tecnologias">
-                        <div> 
-                            <JsLogo />
-                        </div> 
-                        <div>
-                            <ReactLogo/>
                         </div>
+                        <div className="proyecto-tecnologias">
+                            <div> 
+                                <JsLogo />
+                            </div> 
+                            <div>
+                                <ReactLogo/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="proyecto-info">
+                        <h3>Mariposario</h3>
+                        <p>Con motivo de mi trabajo de fin de grado, decidí hacer un prototipo de aplicación web para la mejora de una ya existente. En este caso añadí determinadas funcionales, la más relevante fue una pasarela de pago, y ciertas ajustes en la organización del contenido y apariencia de la interfaz</p>
                     </div>
                 </div>
                 
-                <div>
-                    <h3>Mariposario</h3>
-                    <p>Con motivo de mi trabajo de fin de grado, decidí hacer un prototipo de aplicación web para la mejora de una ya existente. En este caso añadí determinadas funcionales, la más relevante fue una pasarela de pago, y ciertas ajustes en la organización del contenido y apariencia de la interfaz</p>
-                </div>
+
             </article>
 
-            <article className="proyecto-item">
+            {/* <article className="proyecto-item">
                 <div className="proyecto-imagen">
                     <img src="/src/media/pictures/background1.jpg" alt="" />
                 </div>
-                <div className="proyecto-detalles">
+                <div className="proyecto-contenido">
+                    <div className="proyecto-detalles">
                     <div className="proyecto-codigo">
                         <GitCode url="https://github.com/Marg0ti/"/>
                     </div>
@@ -49,6 +53,8 @@ export default function Proyectos(){
                     <h3>Integración de pasarela de pago</h3>
                     <p>De manera más detallada se puede ver de manera práctica el formulario con la redirección a Stripe para proceder al pago.</p>
                 </div>
+                </div>
+
             </article>
 
             <article className="proyecto-item">
@@ -97,7 +103,7 @@ export default function Proyectos(){
                     <h3>Aplicación web con MVC con .NET Core y Entity Framework</h3>
                     <p>Aplicación desarrollada con el patrón Modelo-Vista-Controlador. Permite realizar las distintas operaciones CRUD sobre los artista y sus álbumes, con control de acceso basado en roles asignados a los usuarios.</p>
                 </div>
-            </article>                    
+            </article>                     */}
         </section>
     )
 }
