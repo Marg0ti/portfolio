369650a06d094891327ab220dc98766a6d41e5bf
Marg0ti <165667924+Marg0ti@users.noreply.github.com>
2025-06-01 23:41:00 +0200

    sobre-mi

--- src/pages/sobreMi/SobreMi.jsx
+++ src/pages/sobreMi/SobreMi.jsx
@@ -1,25 +1,27 @@
 export default function SobreMi() {
     return (
         <section id="sobre-mi" >
-                            <h2>Sobre mí</h2>
-            <div className="sobre-mi-texto">
+            <h2>Sobre mí</h2>
+            <div className="sobre-mi-container">
+                <div className="sobre-mi-texto">
 
-                <p>
-                    ¡Hola! Soy Margot y me acabo de graduar en Desarrollo de Aplicaciones Web. 
-                    Me considero una persona entregada, resolutiva y con muchas ganas de aprender y crecer profesionalmente.
-                </p>
-                <p>
-                    Disfruto trabajando en equipo y me emociona la idea de participar en proyectos que supongan un reto y que me permitan seguir mejorando.
-                </p>
+                    <p>
+                        ¡Hola! Soy Margot y me acabo de graduar en Desarrollo de Aplicaciones Web. 
+                        Me considero una persona entregada, resolutiva y con muchas ganas de aprender y crecer profesionalmente.
+                    </p>
+                    <p>
+                        Disfruto trabajando en equipo y me emociona la idea de participar en proyectos que supongan un reto y que me permitan seguir mejorando.
+                    </p>
 
-                <p>
-                    Mi primer contacto con la programación llegó más tarde de lo habitual, 
-                    en un momento en el que me replanteaba si seguir el camino de la carrera que había estudiado.
-                     Gracias a mi entorno y movida por la curiosidad, decidí adentrarme en este nuevo mundo. Descubrí lo apasionante y divertido que es crear aplicaciones y resolver problemas a través del código. Desde entonces, he estado inmersa en el aprendizaje de tecnologías como HTML, CSS, JavaScript, React, .NET, SQL Server, entre otras.
-                </p>
-            </div>
-            <div className="sobre-mi-imagen">
-                <img src="" alt="Margot" />
+                    <p>
+                        Mi primer contacto con la programación llegó más tarde de lo habitual, 
+                        en un momento en el que me replanteaba si seguir el camino de la carrera que había estudiado.
+                        Gracias a mi entorno y movida por la curiosidad, decidí adentrarme en este nuevo mundo. Descubrí lo apasionante y divertido que es crear aplicaciones y resolver problemas a través del código. Desde entonces, he estado inmersa en el aprendizaje de tecnologías como HTML, CSS, JavaScript, React, .NET, SQL Server, entre otras.
+                    </p>
+                </div>
+                <div className="sobre-mi-imagen">
+                    <img src="/src/media/pictures/margot2.jpeg" alt="Margot" />
+                </div>
             </div>
         </section>
     )
