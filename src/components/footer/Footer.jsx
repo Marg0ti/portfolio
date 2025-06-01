ad989567c218864671978f562d99ddfdeafa8884
Marg0ti <165667924+Marg0ti@users.noreply.github.com>
2025-06-02 00:47:01 +0200

    diseno footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,11 @@
+import SocialMedia from "../redes-sociales/SocialMedia";
 export default function Footer() {
       const currentYear = new Date().getFullYear();
     const name= "Margot Hernández González";
     return (
         <footer>
-            <p>© {currentYear} {name} . Todos los derechos reservados.</p>
-            <p>Diseñado y desarrollado por {name}.</p>
-            <p>Imágenes de <a href="https://pixabay.com/" target="_blank" rel="noopener noreferrer">Pixabay</a></p>
+            <p>© {currentYear} {name} </p>
+            <SocialMedia />
         </footer>
     );
 } 
