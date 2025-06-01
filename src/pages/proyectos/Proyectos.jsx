3af9d03189cfcdaa33328fab37b7f44277e53b6f
Marg0ti <165667924+Marg0ti@users.noreply.github.com>
2025-06-01 19:41:52 +0200

    Cambios en el css. Variable de color

--- src/pages/proyectos/Proyectos.jsx
+++ src/pages/proyectos/Proyectos.jsx
@@ -31,79 +31,85 @@ export default function Proyectos(){
 
             </article>
 
-            {/* <article className="proyecto-item">
+            <article className="proyecto-item">
                 <div className="proyecto-imagen">
                     <img src="/src/media/pictures/background1.jpg" alt="" />
                 </div>
-                <div className="proyecto-contenido">
-                    <div className="proyecto-detalles">
-                    <div className="proyecto-codigo">
-                        <GitCode url="https://github.com/Marg0ti/"/>
-                    </div>
-                    <div className="proyecto-tecnologias">
-                        <div> 
-                            <JsLogo />
-                        </div> 
-                        <div>
-                            <ReactLogo/>
+
+                <div className="proyecto-detalles">
+                    <div className="proyecto-detalles2">
+                        <div className="proyecto-codigo">
+                            <GitCode url="https://github.com/Marg0ti/"/>
                         </div>
-                    </div>                    
+                        <div className="proyecto-tecnologias">
+                            <div> 
+                                <JsLogo />
+                            </div> 
+                            <div>
+                                <ReactLogo/>
+                            </div>
+                        </div> 
+                    </div> 
+
+                    <div className="proyecto-info">
+                        <h3 >Integración de pasarela de pago</h3>
+                        <p>De manera más detallada se puede ver de manera práctica el formulario con la redirección a Stripe para proceder al pago.</p>
+                    </div>
                 </div>
-                <div>
-                    <h3>Integración de pasarela de pago</h3>
-                    <p>De manera más detallada se puede ver de manera práctica el formulario con la redirección a Stripe para proceder al pago.</p>
-                </div>
-                </div>
+
 
             </article>
 
-            <article className="proyecto-item">
-                <div className="proyecto-imagen">
-                    <img src="/src/media/pictures/background1.jpg" alt="" />
-                </div>
-                <div className="proyecto-detalles">
-                    <div className="proyecto-codigo">
-                        <GitCode url="https://github.com/Marg0ti/API-music"/>
-                    </div>
-                    <div className="proyecto-tecnologias">
-                        <div> 
-                            <CLogo />
-                        </div> 
-                        <div>
-                            <SQLLogo/>
-                        </div>
-                    </div>
-                </div>
-
+           <article className="proyecto-item">
+    <div className="proyecto-imagen">
+        <img src="/src/media/pictures/background1.jpg" alt="" />
+    </div>
+    <div className="proyecto-detalles">
+        <div className="proyecto-detalles2">
+            <div className="proyecto-codigo">
+                <GitCode url="https://github.com/Marg0ti/API-music" />
+            </div>
+            <div className="proyecto-tecnologias">
+                <div> 
+                    <CLogo />
+                </div> 
                 <div>
-                    <h3>API REST con .NET Core y Entity Framework</h3>
-                    <p>Una aplicación sencilla donde realizar las distintas operaciones CRUD sobre los artistas y sus álbumes.</p>
+                    <SQLLogo />
                 </div>
-            </article>     
+            </div>
+        </div>
+        <div className="proyecto-info">
+            <h3>API REST con .NET Core y Entity Framework</h3>
+            <p>Una aplicación sencilla donde realizar las distintas operaciones CRUD sobre los artistas y sus álbumes.</p>
+        </div>
+    </div>
+</article>
 
-            <article className="proyecto-item">
-                <div className="proyecto-imagen">
-                    <img src="/src/media/pictures/background1.jpg" alt="" />
-                </div>
-                <div className="proyecto-detalles">
-                    <div className="proyecto-codigo">
-                        <GitCode url="https://github.com/Marg0ti/API-music"/>
-                    </div>
-                    <div className="proyecto-tecnologias">
-                        <div> 
-                            <CLogo />
-                        </div> 
-                        <div>
-                            <SQLLogo/>
-                        </div>
-                    </div>
-                </div>
-                
+<article className="proyecto-item">
+    <div className="proyecto-imagen">
+        <img src="/src/media/pictures/background1.jpg" alt="" />
+    </div>
+    <div className="proyecto-detalles">
+        <div className="proyecto-detalles2">
+            <div className="proyecto-codigo">
+                <GitCode url="https://github.com/Marg0ti/API-music" />
+            </div>
+            <div className="proyecto-tecnologias">
+                <div> 
+                    <CLogo />
+                </div> 
                 <div>
-                    <h3>Aplicación web con MVC con .NET Core y Entity Framework</h3>
-                    <p>Aplicación desarrollada con el patrón Modelo-Vista-Controlador. Permite realizar las distintas operaciones CRUD sobre los artista y sus álbumes, con control de acceso basado en roles asignados a los usuarios.</p>
+                    <SQLLogo />
                 </div>
-            </article>                     */}
+            </div>
+        </div>
+        <div className="proyecto-info">
+            <h3>Aplicación web con MVC con .NET Core y Entity Framework</h3>
+            <p>Aplicación desarrollada con el patrón Modelo-Vista-Controlador. Permite realizar las distintas operaciones CRUD sobre los artista y sus álbumes, con control de acceso basado en roles asignados a los usuarios.</p>
+        </div>
+    </div>
+</article>
+             
         </section>
     )
 }
